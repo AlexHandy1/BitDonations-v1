44937cda528c0de30194d8dc31fb31ce423dd525
Alex Handy <handy437@gmail.com>
2015-06-02 18:40:37 +0100

    Completed basic, manual bitcoin receipt through API

--- app/controllers/donor.rb
+++ app/controllers/donor.rb
@@ -6,7 +6,6 @@ end
 post '/donor' do
   name, email, password, password_confirmation = params['name'], params['email'], params['password'], params['password_confirmation']
   @donor = Donor.create(name: name, email: email, password: password, password_confirmation: password_confirmation)
-  #test - will implement in sessions
   @new_donor = Donor.first(@donor.name)
 
   if @donor.save
