4c06ac86457e6780101b2828e917b9706c943424
Alex Handy <handy437@gmail.com>
2015-06-02 12:47:07 +0100

    Successfully added name, now dynamic email test Heroku

--- app/controllers/donor.rb
+++ app/controllers/donor.rb
@@ -7,7 +7,7 @@ post '/donor' do
   name, email, password, password_confirmation = params['name'], params['email'], params['password'], params['password_confirmation']
   @donor = Donor.create(name: name, email: email, password: password, password_confirmation: password_confirmation)
   #test - will implement in sessions
-  @current_donor = Donor.first(@donor.name)
+  @new_donor = Donor.first(@donor.name)
 
   if @donor.save
     send_confirmation_email
--- app/helpers/confirmation_email.rb
+++ app/helpers/confirmation_email.rb
@@ -5,8 +5,8 @@ helpers do
     RestClient.post 'https://api:key-3ax6xnjp29jd6fds4gc373sgvjxteol0'\
         '@api.mailgun.net/v2/samples.mailgun.org/messages',
       from: "Mailgun Sandbox <postmaster@sandbox29795a797e5b4c75a3a5375cb657ff3a.mailgun.org>",
-      to: "Alex Handy <handy437@gmail.com>",
+      to: @new_donor.email,
       subject: 'Congratulations on signing-up to BitDonations',
-      text:'Welcome' + @current_donor.name + 'you can now donate to entrepreneurs directly using Bitcoin'
+      text:'Welcome ' + @new_donor.name + ' you can now donate to entrepreneurs directly using Bitcoin'
   end
 end
