4085943dab58cf4d6c9dc00e2187de2bf5f0aa57
Alex Handy <handy437@gmail.com>
2015-06-03 18:41:22 +0100

    Styled entrepreneur project page

--- app/controllers/donate.rb
+++ app/controllers/donate.rb
@@ -38,16 +38,6 @@ post '/donate/:id/new' do
   end
 end
 
-#Step 1 - get a manual, hardcoded 'send' request linked to my addresses working (hardcode amount etc) that is invoked when click Donate button
-  #donor clicks on Donate button taken to Make a Donation page
-    # >> simplest version would be to manually load my donor wallet address 1 as recipient(eventually call receive API on loaded wallet to get a randomely generated address) and then put in an address wallet address 2 that I create for sender
-    # payment = wallet.send([recipient - entrepreneur]'1NAF7GbdyRg3miHNrw2bGxrd63tfMEmJob', [amount]1000000, [from_address - donor]: '1A8JiWcwvpY7tAopUkSnGuEYHmzGYfZPiq')
-      # >> could send this with a hidden form underneath button which posts to donation/new and runs the send method whilst also showing a thank you for donation, click to go back to home-page
-#Step 2 - donor completes form where they dynamically put in amount and their blockchain wallet address which they want to send from
-#Step 3 - dynamically create blockchain wallets (with API) when donors and entrepreneurs sign-up and use convenience methods e.g. new_address, get_address to populate required fields
-#Step 4 - add a confirmation step which checks transaction status and calls a Twilio text method (consider moving this up) to send a text
-#Step 5 - add in a whole set-up and storage process to create wallet for either party so not brand new with every single transaction, actually want to invite users to set-up and then store?
-
 
 
 
--- app/controllers/ent.rb
+++ app/controllers/ent.rb
@@ -1,4 +1,5 @@
 post '/ent/:id' do
   @ent_id = params['ent_id']
+  @ent = Entrepreneur.get(@ent_id)
   erb :'ent/id1'
 end
